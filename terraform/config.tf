acfac19d91a77b84187add9e0693ede2bb3bc79e
Alwyn Kik <alwyn@kik.pw>
2022-01-04 23:34:18 +0100

    Initial commit for terraform

---
+++ terraform/config.tf
@@ -0,0 +1,46 @@
+# User configuration
+variable "volume_teamspeak_data1_size" {
+  default   = 10 # GB, 10 is the minimum
+}
+
+# Some defaults
+variable "hetzner_default_image" {
+  default   = "ubuntu-20.04"
+}
+variable "hetzner_default_location" {
+  default   = "hel1"
+}
+variable "hetzner_default_server_type" {
+  default   = "cpx11"
+}
+variable "dns_default_ttl" {
+  default   = 120
+}
+
+# Default SSH keys
+data "hcloud_ssh_keys" "all_keys" {
+}
+
+# Configuration of the Hetzner Cloud terraform provider
+terraform {
+  required_providers {
+    hcloud = {
+      source = "hetznercloud/hcloud"
+      version = "1.32.2"
+    }
+    gandi = {
+      version = "2.0.0-rc3"
+      source = "github/go-gandi/gandi"
+    }
+  }
+}
+
+provider "hcloud" {
+  # Configuration options
+}
+
+#provider "gandi" {
+#  key         = ""
+#  sharing_id  = ""
+#}
+
---
+++ terraform/infra.tf
@@ -0,0 +1,56 @@
+#####################
+### Hetzner Cloud ###
+#####################
+
+# Documentation:
+#   https://registry.terraform.io/providers/hetznercloud/hcloud/latest/docs
+
+resource "hcloud_server" "teamspeak" {
+  name        = "teamspeak"
+  image       = var.hetzner_default_image
+  location    = var.hetzner_default_location
+  server_type = var.hetzner_default_server_type
+  ssh_keys    = data.hcloud_ssh_keys.all_keys.ssh_keys.*.name
+}
+
+resource "hcloud_volume" "teamspeak_data" {
+  name              = "teamspeakdata1"
+  size              = var.volume_teamspeak_data1_size
+  format            = "ext4"
+  delete_protection = true
+}
+
+resource "hcloud_volume_attachment" "teamspeak_data_mount" {
+  volume_id = hcloud_volume.teamspeak_data.id
+  server_id = hcloud_server.teamspeak.id
+  automount = true
+}
+
+###########
+### DNS ###
+###########
+
+## The domain
+#data "gandi_livedns_domain" "tora" {
+#  name          = "tora.games"
+#}
+#
+#### DNS records ###
+#
+## teamspeak
+#resource "gandi_livedns_record" "ts_ipv4" {
+#  zone          = data.gandi_livedns_domain.tora.id
+#  name          = "ts"
+#  type          = "A"
+#  value         = hcloud_server.teamspeak.ipv4_address
+#  ttl           = var.dns_default_ttl
+#}
+#
+#resource "gandi_livedns_record" "ts_ipv6" {
+#  zone          = data.gandi_livedns_domain.tora.id
+#  name          = "ts"
+#  type          = "AAAA"
+#  value         = hcloud_server.teamspeak.ipv6_address
+#  ttl           = var.dns_default_ttl
+#}
+
